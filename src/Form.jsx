981b6a64d1bd4d12db5db27afd4536121e984dc5
okyws <okywahyu1@gmail.com>
2022-11-23 09:30:57 +0700

    Refactor: refactor code in form

--- src/Form.jsx
+++ src/Form.jsx
@@ -7,25 +7,26 @@ import Label from './components/Label';
 import PlaceContentCenter from './components/PlaceContentCenter';
 
 export default function Form() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+  });
   return (
     <PlaceContentCenter>
       <Card>
         <Card.Title>Sign Up</Card.Title>
         <Card.Body>
           <div className='mb-5 border rounded-lg p-4'>
-            <p>name: {name || '----'}</p>
-            <p>email: {email || '----'}</p>
+            <p>name: {form.name || '----'}</p>
+            <p>email: {form.email || '----'}</p>
           </div>
           <div className='mb-6'>
             <Label htmlFor='name' value={'Name'} />
-            <Input value={name} onChange={(e) => setName(e.target.value)} name={'name'} id={'name'} />
+            <Input value={form.name} onChange={(e) => setForm({ ...form, name: e.target.value })} name={'name'} id={'name'} />
           </div>
           <div className=''>
             <Label htmlFor='email' value={'Email'} />
-            <Input value={email} onChange={(e) => setEmail(e.target.value)} name={'email'} id={'email'} />
+            <Input value={form.email} onChange={(e) => setForm((form) => ({ ...form, email: e.target.value }))} name={'email'} id={'email'} />
           </div>
         </Card.Body>
         <Card.Footer>
