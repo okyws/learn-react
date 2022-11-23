cbdae601ee81e790bb93ac8b18d4b43638658796
okyws <okywahyu1@gmail.com>
2022-11-23 09:40:45 +0700

    Refactor: code in better way

--- src/Form.jsx
+++ src/Form.jsx
@@ -11,30 +11,42 @@ export default function Form() {
     name: '',
     email: '',
   });
+
+  function onChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function submit(e) {
+    e.preventDefault();
+    console.log(form);
+  }
+
   return (
     <PlaceContentCenter>
       <Card>
         <Card.Title>Sign Up</Card.Title>
-        <Card.Body>
-          <div className='mb-5 border rounded-lg p-4'>
-            <p>name: {form.name || '----'}</p>
-            <p>email: {form.email || '----'}</p>
-          </div>
-          <div className='mb-6'>
-            <Label htmlFor='name' value={'Name'} />
-            <Input value={form.name} onChange={(e) => setForm({ ...form, name: e.target.value })} name={'name'} id={'name'} />
-          </div>
-          <div className=''>
-            <Label htmlFor='email' value={'Email'} />
-            <Input value={form.email} onChange={(e) => setForm((form) => ({ ...form, email: e.target.value }))} name={'email'} id={'email'} />
-          </div>
-        </Card.Body>
-        <Card.Footer>
-          <Button className='bg-black'>
-            <IconBrandGithub />
-            Sign Up
-          </Button>
-        </Card.Footer>
+        <form onSubmit={submit}>
+          <Card.Body>
+            <div className='mb-5 border rounded-lg p-4'>
+              <p>name: {form.name || '----'}</p>
+              <p>email: {form.email || '----'}</p>
+            </div>
+            <div className='mb-6'>
+              <Label htmlFor='name' value={'Name'} />
+              <Input value={form.name} onChange={onChange} name={'name'} id={'name'} />
+            </div>
+            <div className=''>
+              <Label htmlFor='email' value={'Email'} />
+              <Input value={form.email} onChange={onChange} name={'email'} id={'email'} />
+            </div>
+          </Card.Body>
+          <Card.Footer>
+            <Button className='bg-black'>
+              <IconBrandGithub />
+              Sign Up
+            </Button>
+          </Card.Footer>
+        </form>
       </Card>
     </PlaceContentCenter>
   );
